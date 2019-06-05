3e7b6e580ac7dfc5e73c5399869f04656f9547a2
maxb2 <maxb2@mail.missouri.edu>
2019-06-05 17:00:32 -0500

    Add argument parser

--- pytiler.py
+++ pytiler.py
@@ -1,15 +1,26 @@
 #!/usr/bin/python
 
 from PIL import Image
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("tiles", metavar='dims', nargs=2, type=int, help="number of tiles in each direction")
+# parser.add_argument("xtiles", type=int, help="number of tiles in the x direction")
+# parser.add_argument("ytiles", type=int, help="number of tiles in the y direction")
+parser.add_argument("f_in", metavar='files', type=str, nargs='+', help="input images to be tiled")
+parser.add_argument("-o", "--out", type=str, help="output image", default="out.png")
+args = parser.parse_args()
 
+xtiles, ytiles = args.tiles
+files_in = args.f_in[0]
 
-xtiles=4
-ytiles=
+print(args)
+print(xtiles,ytiles,files_in)
 
-img = Image.open('test.png', 'r')
+img = Image.open(files_in, 'r')
 img_w, img_h = img.size
 tile_w=img_w//3
-background = Image.new('RGBA', (xtiles*tile_w+(img_w-tile_w), img_h), (0, 0, 0, 0))
+tile_h=img_h//3
+background = Image.new('RGBA', (xtiles*tile_w+(img_w-tile_w), (ytiles*tile_h+(img_h-tile_h))), (0, 0, 0, 0))
 bg_w, bg_h = background.size
 #offset = ((bg_w - img_w) // 2, (bg_h - img_h) // 2)
 #background.paste(img, offset)
@@ -18,7 +29,9 @@ bg_w, bg_h = background.size
 comp=background
 for i in range(xtiles):
     xoffset=i*tile_w
-    new=Image.new('RGBA', (xtiles*tile_w+(img_w-tile_w), img_h), (0, 0, 0, 0))
-    new.paste(img.rotate(i*90), (xoffset,0))
-    comp=Image.alpha_composite(comp,new)
-comp.save('out.png')
+    for j in range(ytiles):
+        yoffset=j*tile_h
+        new=Image.new('RGBA', (xtiles*tile_w+(img_w-tile_w), (ytiles*tile_h+(img_h-tile_h))), (0, 0, 0, 0))
+        new.paste(img.rotate(((i+1)*(j+1)-1)*90), (xoffset,yoffset))
+        comp=Image.alpha_composite(comp,new)
+comp.save(args.out)
