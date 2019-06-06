93f8d6d59aaffc1282593ec32f81f8823f64e0fd
maxb2 <maxb2@mail.missouri.edu>
2019-06-06 10:34:13 -0500

    Add random selection and rotation

--- pytiler.py
+++ pytiler.py
@@ -1,37 +1,33 @@
 #!/usr/bin/python
 
 from PIL import Image
+from random import randint, choice
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("tiles", metavar='dims', nargs=2, type=int, help="number of tiles in each direction")
-# parser.add_argument("xtiles", type=int, help="number of tiles in the x direction")
-# parser.add_argument("ytiles", type=int, help="number of tiles in the y direction")
-parser.add_argument("f_in", metavar='files', type=str, nargs='+', help="input images to be tiled")
+parser.add_argument("f_in", metavar='files', type=str, nargs='+', help="input images to be tiled, randomly selected")
 parser.add_argument("-o", "--out", type=str, help="output image", default="out.png")
+parser.add_argument("-t", "--tile-divs", type=int, help="tile subdivisions, usually to take care of alpha borders", default=1)
+parser.add_argument("-r", "--rotate", help="randomly rotate the tiles", action="store_true")
 args = parser.parse_args()
 
 xtiles, ytiles = args.tiles
-files_in = args.f_in[0]
+files_in = args.f_in
 
-print(args)
-print(xtiles,ytiles,files_in)
+imgs = [Image.open(f, 'r') for f in files_in ]
+img_w, img_h = imgs[0].size
+tile_w=img_w//args.tile_divs
+tile_h=img_h//args.tile_divs
+comp = Image.new('RGBA', (xtiles*tile_w+(img_w-tile_w), (ytiles*tile_h+(img_h-tile_h))), (0, 0, 0, 0))
 
-img = Image.open(files_in, 'r')
-img_w, img_h = img.size
-tile_w=img_w//3
-tile_h=img_h//3
-background = Image.new('RGBA', (xtiles*tile_w+(img_w-tile_w), (ytiles*tile_h+(img_h-tile_h))), (0, 0, 0, 0))
-bg_w, bg_h = background.size
-#offset = ((bg_w - img_w) // 2, (bg_h - img_h) // 2)
-#background.paste(img, offset)
-#background.save('out.png')
-
-comp=background
 for i in range(xtiles):
     xoffset=i*tile_w
     for j in range(ytiles):
         yoffset=j*tile_h
         new=Image.new('RGBA', (xtiles*tile_w+(img_w-tile_w), (ytiles*tile_h+(img_h-tile_h))), (0, 0, 0, 0))
-        new.paste(img.rotate(((i+1)*(j+1)-1)*90), (xoffset,yoffset))
-        comp=Image.alpha_composite(comp,new)
+        img = choice(imgs)
+        if args.rotate:
+            img = img.rotate(randint(0,3)*90)
+        new.paste(img, (xoffset,yoffset))
+        comp = Image.alpha_composite(comp,new)
 comp.save(args.out)
